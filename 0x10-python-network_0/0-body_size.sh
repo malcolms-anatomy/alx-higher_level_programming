3a156c8bda2c4acf9fa1a17d86b9de48642f038c
malcolms-anatomy <malcolmihere@outlook.com>
2024-07-21 10:21:05 -0700

    Add 0-body_size.sh script and README.md

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Sends a request to the URL and displays the size of the body of the response in bytes
+# Get the byte size of the HTTP response header for a given URL.
 curl -s "$1" | wc -c
-
