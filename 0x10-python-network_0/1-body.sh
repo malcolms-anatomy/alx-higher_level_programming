e32dc82d5167d1b40e3091d3d3ecbf3d851125c0
Malcolm Chikadibia Iheremelam <malcolmihere@outlook.com>
2024-09-27 21:54:54 +0100

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/1-body.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Sends a GET request to the URL and displays the body of a 200 status code response
-curl -sL -w "%{http_code}" "$1" -o response.txt | grep -q "200" && cat response.txt
